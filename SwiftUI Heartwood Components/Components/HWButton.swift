8875632b99dd637cd8a37fee6384cedf6d97091b
Ricky Padilla <rickycpadilla@gmail.com>
2019-08-28 14:35:15 -0600

    Added loading and disabled states to button.

--- SwiftUI Heartwood Components/Components/HWButton.swift
+++ SwiftUI Heartwood Components/Components/HWButton.swift
@@ -53,20 +53,37 @@ struct HWButton: View {
     return HWStyles.dynamicColor(dynamicColor)
   }
   
+  private var contentColor: Color {
+    return model.kind == .primary ? neutralColor : buttonColor
+  }
+  
+  // NOTE: If Apple does not exposed an `onPress`-like method, we'll need
+  // to override the onPress BG color in the less ideal way as shown at:
+  // https://stackoverflow.com/questions/56509640/how-to-set-custom-highlighted-state-of-swiftui-button
+  
   var body: some View {
     Button(action: model.action) {
       HStack(alignment: .center) {
         if model.isFullWidth {
           Spacer()
         }
-        if model.icon != nil {
-          HWIcon(name: model.icon!)
-            .foregroundColor(model.kind == .primary ? neutralColor : buttonColor)
-        }
-        if model.title != nil {
-          Text(model.title ?? "")
-            .font(titleFont)
-            .foregroundColor(model.kind == .primary ? neutralColor : buttonColor)
+        ZStack {
+          if model.isLoading {
+            HWLoader()
+              .frame(width: 20, height: 20, alignment: .center)
+              .foregroundColor(contentColor)
+          }
+          HStack {
+            if model.icon != nil {
+              HWIcon(name: model.icon!)
+                .foregroundColor(model.isLoading ? .clear : contentColor)
+            }
+            if model.title != nil {
+              Text(model.title ?? "")
+                .font(titleFont)
+                .foregroundColor(model.isLoading ? .clear : contentColor)
+            }
+          }
         }
         if model.isFullWidth {
           Spacer()
@@ -80,7 +97,8 @@ struct HWButton: View {
         RoundedRectangle(cornerRadius: 4)
           .stroke(model.kind == .secondary ? buttonColor : .clear, lineWidth: 1)
       )
-    }
+        .opacity(model.isDisabled ? 0.5 : 1.0)
+    }.disabled(model.isLoading || model.isDisabled)
   }
 }
 
@@ -91,11 +109,11 @@ struct HWButton_Previews: PreviewProvider {
         action: {},
         title: "Add a guest",
         icon: HeartwoodTokens.Icon.add,
-//        kind: .secondary,
-//        colorKind: .success,
-        isDisabled: false,
-        isLoading: false,
-        isFullWidth: true
+        kind: .secondary,
+        colorKind: .success,
+//        isDisabled: true,
+//        isLoading: true,
+        isFullWidth: false
       )
     return HWButton(model: model).padding()
   }
